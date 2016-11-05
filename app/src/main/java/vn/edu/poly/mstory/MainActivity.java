3b32da19a5c8a9b256865b77addf5286539235e1
gdcvn <vuongphan04467@gmail.com>
2016-11-05 18:40:36 +0700

    ver 0.0.1 first

---
+++ app/src/main/java/vn/edu/poly/mstory/MainActivity.java
@@ -0,0 +1,20 @@
+package vn.edu.poly.mstory;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        if (savedInstanceState == null) {
+//            getSupportFragmentManager().beginTransaction()
+//                    .add(R.id.fragment, new PlaceholderFragment())
+//                    .commit();
+//        }
+
+    }
+}

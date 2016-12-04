2cb2848c46894ef811d2f7a57ccb1bfbffc49c60
VanTuTrieu <vantutrieu97@gmail.com>
2016-12-04 18:02:47 +0700

    z

--- app/src/main/java/vn/edu/poly/mcomics/object/handle/other/NavigationDrawer.java
+++ app/src/main/java/vn/edu/poly/mcomics/object/handle/other/NavigationDrawer.java
@@ -1,23 +1,19 @@
 package vn.edu.poly.mcomics.object.handle.other;
 
 import android.app.Activity;
-import android.app.Application;
 import android.app.Dialog;
-import android.app.ListActivity;
 import android.content.Context;
-import android.content.Intent;
-import android.os.Bundle;
 import android.provider.Settings;
-import android.support.v4.view.LayoutInflaterCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.text.Layout;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -32,12 +28,13 @@ import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
 import vn.edu.poly.mcomics.R;
+import vn.edu.poly.mcomics.object.handle.social.FacebookAPI;
 
 /**
  * Created by lucius on 11/16/16.
  */
 
-public class NavigationDrawer{
+public class NavigationDrawer {
     private LayoutInflater inflater;
     private ViewGroup parent;
     private View view;
@@ -46,26 +43,28 @@ public class NavigationDrawer{
     private CallbackManager callbackManager;
     private Toolbar toolbar;
     private DrawerLayout drawerLayout;
+    private FacebookAPI facebookAPI;
+
 
     public NavigationDrawer(final Activity activity, int layout, ViewGroup viewGroup) {
         this.activity = activity;
         this.inflater = (LayoutInflater.from(activity));
         this.parent = viewGroup;
+        facebookAPI = new FacebookAPI(activity);
+        facebookAPI.init();
 
         view = (inflater.inflate(layout, parent, false));
 
-        ((AppCompatActivity)activity).getSupportActionBar().hide();
+        ((AppCompatActivity) activity).getSupportActionBar().hide();
 
         toolbar = (Toolbar) inflater.inflate(R.layout.toolbar, ((ViewGroup) view), false)
                 .findViewById(R.id.toolBar);
 
-        ((LinearLayout)view).addView(toolbar, 0);
+        ((LinearLayout) view).addView(toolbar, 0);
         ((FrameLayout) parent.findViewById(R.id.root)).addView(view);
-
-        drawerLayout = ((DrawerLayout)parent);
-
+        drawerLayout = ((DrawerLayout) parent);
         callbackManager = CallbackManager.Factory.create();
-        LoginButton loginButton = (LoginButton)parent.findViewById(R.id.fb_login);
+        LoginButton loginButton = (LoginButton) parent.findViewById(R.id.fb_login);
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
@@ -76,16 +75,21 @@ public class NavigationDrawer{
             public void onCancel() {
 
             }
-//
+
+            //
             @Override
             public void onError(FacebookException error) {
                 Toast.makeText(activity, "Đăng nhập thất bại", Toast.LENGTH_SHORT).show();
             }
         });
+        //
+        //
+        //
 
         TextView change = (TextView) parent.findViewById(R.id.change);
-
         clickBrightness();
+        change.setText("Độ sáng :" + Settings.System.getInt(activity.getContentResolver(), Settings
+                .System.SCREEN_BRIGHTNESS, 0) * 100 / 255 + " %");
         change.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -93,12 +97,16 @@ public class NavigationDrawer{
                 dialog.show();
             }
         });
-
+        ((EditText) toolbar.findViewById(R.id.ed_search)).setVisibility(View.INVISIBLE);
+        beforeIconSearchClick();
         createNavifationButton();
+        clickSearch();
+        beforeIconSearchClick();
+
     }
 
-    public void createNavifationButton(){
-        ((ImageView)toolbar.findViewById(R.id.navigation_button)).setOnClickListener(new View.OnClickListener() {
+    public void createNavifationButton() {
+        ((ImageView) toolbar.findViewById(R.id.navigation_button)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 showHideNavigation();
@@ -106,13 +114,58 @@ public class NavigationDrawer{
         });
     }
 
-    public void showHideNavigation(){
-        if(drawerLayout.isDrawerOpen(Gravity.START))
+    public void showHideNavigation() {
+        if (drawerLayout.isDrawerOpen(Gravity.START))
             drawerLayout.closeDrawer(Gravity.START);
         else
             drawerLayout.openDrawer(Gravity.START);
     }
 
+    public void clickSearch() {
+        ((ImageView) toolbar.findViewById(R.id.img_v_search)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((TextView) toolbar.findViewById(R.id.txv_AppName)).setVisibility(View.INVISIBLE);
+                ((EditText) toolbar.findViewById(R.id.ed_search)).setVisibility(View.VISIBLE);
+                // khi nhan vao nt tim kiem thi ban phim ao hien len
+                InputMethodManager keyBoard = (InputMethodManager) activity.getSystemService(Context
+                        .INPUT_METHOD_SERVICE);
+                keyBoard.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
+                ((ImageView) toolbar.findViewById(R.id.img_v_search)).setVisibility(View.INVISIBLE);
+                ((ImageView) toolbar.findViewById(R.id.img_v_searched)).setVisibility(View.VISIBLE);
+                ((ImageView) toolbar.findViewById(R.id.img_v_searched)).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        afterIconSearchClicked();
+                    }
+                });
+
+            }
+        });
+    }
+
+    public void beforeIconSearchClick() {
+        ((ImageView) toolbar.findViewById(R.id.img_v_search)).setVisibility(View.VISIBLE);
+        ((ImageView) toolbar.findViewById(R.id.img_v_searched)).setVisibility(View.INVISIBLE);
+    }
+
+    public void afterIconSearchClicked() {
+
+        if (((EditText) toolbar.findViewById(R.id.ed_search)).getText().length() > 0) {
+            Toast.makeText(activity, ((EditText) toolbar.findViewById(R.id.ed_search)).getText(),
+                    Toast.LENGTH_SHORT).show();
+        }
+        ((ImageView) toolbar.findViewById(R.id.img_v_search)).setVisibility(View.VISIBLE);
+        ((ImageView) toolbar.findViewById(R.id.img_v_searched)).setVisibility(View.INVISIBLE);
+        ((TextView) toolbar.findViewById(R.id.txv_AppName)).setVisibility(View.VISIBLE);
+        ((EditText) toolbar.findViewById(R.id.ed_search)).setVisibility(View.INVISIBLE);
+        ((EditText) toolbar.findViewById(R.id.ed_search)).setText(null);
+        // khi nhan tim kiem sau khi nhap du lieu thi ban phim ao bien mat
+        InputMethodManager keyBoard = (InputMethodManager) activity.getSystemService(Context
+                .INPUT_METHOD_SERVICE);
+        keyBoard.toggleSoftInput(InputMethodManager.HIDE_NOT_ALWAYS, 0);
+        //Code thuc hien tim kiem truyen o duoi day
+    }
     public void clickBrightness() {
         dialog = new Dialog(activity);
         dialog.setTitle("Thay đổi độ sáng");
@@ -122,6 +175,9 @@ public class NavigationDrawer{
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 Settings.System.putInt(activity.getContentResolver(), Settings.System.SCREEN_BRIGHTNESS, progress);
+                TextView change = (TextView) parent.findViewById(R.id.change);
+                change.setText("Độ sáng :" + progress * 100 / 255 + " %");
+
             }
 
             @Override

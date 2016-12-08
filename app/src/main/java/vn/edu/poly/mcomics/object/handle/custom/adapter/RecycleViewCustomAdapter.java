b9a793e9c646540734d9d58998d985562a8cbe3c
lucius165 <spnova165@gmail.com>
2016-12-09 05:16:44 +0700

    fix some wrong

--- app/src/main/java/vn/edu/poly/mcomics/object/handle/custom/adapter/RecycleViewCustomAdapter.java
+++ app/src/main/java/vn/edu/poly/mcomics/object/handle/custom/adapter/RecycleViewCustomAdapter.java
@@ -1,302 +1,82 @@
-package vn.edu.poly.mcomics.object.handle.other;
+package vn.edu.poly.mcomics.object.handle.custom.adapter;
 
-import android.Manifest;
-import android.app.Activity;
-import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Color;
-import android.graphics.Point;
-import android.net.Uri;
-import android.os.Build;
-import android.provider.Settings;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.SeekBar;
-import android.widget.TableRow;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.facebook.AccessToken;
-import com.facebook.AccessTokenTracker;
-import com.facebook.FacebookCallback;
-import com.facebook.FacebookException;
-import com.facebook.login.LoginResult;
-import com.facebook.login.widget.LoginButton;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
 
 import vn.edu.poly.mcomics.R;
-import vn.edu.poly.mcomics.object.handle.social.FacebookAPI;
+import vn.edu.poly.mcomics.activity.ComicDetailActivity;
+import vn.edu.poly.mcomics.object.variable.Comics;
 
 /**
- * Created by lucius on 11/16/16.
+ * Created by lucius on 11/15/16.
  */
 
-public class NavigationDrawer implements View.OnClickListener {
-    private LayoutInflater inflater;
-    private ViewGroup parent;
-    private View mainView;
-    private Activity activity;
-    private Dialog brightnessDialog, viewModeDialog;
-    private Toolbar toolbar;
-    private DrawerLayout drawerLayout;
-    private FacebookAPI facebookAPI;
-    private LoginButton loginButton;
-    private TextView txv_log;
-    private LinearLayout btn_vertical, btn_horizontal;
-    private SettingHandle settingHandle;
-    protected SeekBar seekBar;
+public class RecycleViewCustomAdapter extends RecyclerView.Adapter<RecycleViewCustomAdapter.RecyclerViewHolder> {
 
-    private final int REQUEST_CODE = 200;
-    private TableRow btn_likes, btn_shares, btn_log, btn_brightness, btn_viewMode;
-    private final float ONE_PERCENT = 255f / 100f;
+    private ArrayList<Comics> arrComics = new ArrayList<>();
 
-    public NavigationDrawer(final Activity activity, int layout, ViewGroup viewGroup) {
-        this.activity = activity;
-        this.inflater = (LayoutInflater.from(activity));
-        this.parent = viewGroup;
-        mappings(layout);
-
-        ((LinearLayout) mainView).addView(toolbar, 0);
-        ((FrameLayout) parent.findViewById(R.id.root)).addView(mainView);
-
-        createFbLoginButton();
-        setButtonOnClick();
-        createBrightnessDialog();
-        createViewModeDialog();
-
-        new AccessTokenTracker() {
-            @Override
-            protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
-                checkLogin();
-            }
-        };
-
-        createNavigationButton();
-    }
-
-    public void mappings(int layout) {
-        facebookAPI = new FacebookAPI(activity);
-        facebookAPI.init();
-        toolbar = (Toolbar) inflater.inflate(R.layout.view_toolbar, ((ViewGroup) mainView), false).findViewById(R.id.toolBar);
-        drawerLayout = ((DrawerLayout) parent);
-        mainView = (inflater.inflate(layout, parent, false));
-        loginButton = (LoginButton) parent.findViewById(R.id.fb_login);
-        btn_likes = (TableRow) parent.findViewById(R.id.btn_like);
-        btn_shares = (TableRow) parent.findViewById(R.id.btn_share);
-        btn_log = (TableRow) parent.findViewById(R.id.btn_log);
-        btn_brightness = (TableRow) parent.findViewById(R.id.btn_brightness);
-        btn_viewMode = (TableRow) parent.findViewById(R.id.btn_viewMode);
-        txv_log = (TextView) parent.findViewById(R.id.txv_log);
-        settingHandle = new SettingHandle(activity);
-    }
-
-    public void setButtonOnClick() {
-        btn_likes.setOnClickListener(this);
-        btn_likes.setOnClickListener(this);
-        btn_log.setOnClickListener(this);
-        btn_brightness.setOnClickListener(this);
-        btn_viewMode.setOnClickListener(this);
-    }
-
-    public void getPermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (activity.checkSelfPermission(Manifest.permission.WRITE_SETTINGS) != PackageManager.PERMISSION_GRANTED) {
-                if (Settings.System.canWrite(activity)) {
-                    ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.WRITE_SETTINGS}, REQUEST_CODE);
-                } else {
-                    Intent intent = new Intent(android.provider.Settings.ACTION_MANAGE_WRITE_SETTINGS);
-                    intent.setData(Uri.parse("package:" + activity.getPackageName()));
-                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    activity.startActivity(intent);
-                }
-            }
-        }
-    }
-
-    public int getScreenWidth() {
-        Point point = new Point();
-        activity.getWindowManager().getDefaultDisplay().getSize(point);
-        return point.x;
-    }
-
-    public int getScreenHeight() {
-        Point point = new Point();
-        activity.getWindowManager().getDefaultDisplay().getSize(point);
-        return point.y;
-    }
-
-    public void checkLogin() {
-        if (facebookAPI.isLogged()) {
-            btn_likes.setAlpha(1);
-            btn_shares.setAlpha(1);
-            txv_log.setText("Đăng xuất");
-        } else {
-            btn_likes.setAlpha(0.7f);
-            btn_shares.setAlpha(0.7f);
-            txv_log.setText("Đăng Nhập");
-        }
-    }
-
-    public void createBrightnessDialog() {
-        getPermission();
-
-        brightnessDialog = new Dialog(activity);
-        brightnessDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        brightnessDialog.setContentView(R.layout.view_dialog_brightness);
-
-        ((LinearLayout) brightnessDialog.findViewById(R.id.dialog_brightness)).getLayoutParams().width = (int) (getScreenWidth() * 0.90);
-
-        seekBar = (SeekBar) brightnessDialog.findViewById(R.id.sbBrightness);
-        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                Settings.System.putInt(activity.getContentResolver(), Settings.System.SCREEN_BRIGHTNESS, progress);
-                ((TextView) brightnessDialog.findViewById(R.id.txv_currentBrightness)).setText((int) (progress / ONE_PERCENT) + "");
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });
-
-    }
-
-    public void createViewModeDialog() {
-        viewModeDialog = new Dialog(activity);
-        viewModeDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        viewModeDialog.setContentView(R.layout.view_dialog_swipe_mode);
-        ((LinearLayout) viewModeDialog.findViewById(R.id.dialog_viewMode)).getLayoutParams().width = (int) (getScreenWidth() * 0.90);
-
-        btn_vertical = (LinearLayout) viewModeDialog.findViewById(R.id.ll_btn_Vertical);
-        btn_horizontal = (LinearLayout) viewModeDialog.findViewById(R.id.ll_btn_Horizontal);
-        refreshViewMode();
-
-        btn_vertical.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Show.log("btn_vertical", LinearLayoutManager.VERTICAL + "");
-                settingHandle.setOrientation(LinearLayoutManager.VERTICAL);
-                refreshViewMode();
-            }
-        });
-
-        btn_horizontal.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Show.log("btn_horizontal", LinearLayoutManager.HORIZONTAL + "");
-                settingHandle.setOrientation(LinearLayoutManager.HORIZONTAL);
-                refreshViewMode();
-            }
-        });
-    }
-
-    public void refreshViewMode() {
-        clearBorderViewMode();
-        if (settingHandle.getOrientation() == LinearLayoutManager.VERTICAL) {
-            btn_vertical.setBackgroundColor(Color.parseColor("#000000"));
-        } else {
-            btn_horizontal.setBackgroundColor(Color.parseColor("#000000"));
-        }
-        Show.log("orientation", settingHandle.getOrientation() + "");
-    }
-
-    public void clearBorderViewMode() {
-        btn_vertical.setBackgroundColor(Color.parseColor("#FFFFFF"));
-        btn_horizontal.setBackgroundColor(Color.parseColor("#FFFFFF"));
-    }
-
-    public void createFbLoginButton() {
-        facebookAPI.createLoginButton(loginButton,
-                new FacebookCallback<LoginResult>() {
-                    @Override
-                    public void onSuccess(LoginResult loginResult) {
-                        Toast.makeText(activity, "Đăng nhập thành công", Toast.LENGTH_SHORT).show();
-                    }
-
-                    @Override
-                    public void onCancel() {
-
-                    }
-
-                    @Override
-                    public void onError(FacebookException error) {
-                        Toast.makeText(activity, "Đăng nhập thất bại", Toast.LENGTH_SHORT).show();
-                    }
-                });
-        checkLogin();
-    }
-
-    public void createNavigationButton() {
-        ((ImageView) toolbar.findViewById(R.id.navigation_button)).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showHideNavigation();
-            }
-        });
-    }
-
-    public void showHideNavigation() {
-        if (drawerLayout.isDrawerOpen(GravityCompat.START))
-            drawerLayout.closeDrawer(GravityCompat.START);
-        else
-            drawerLayout.openDrawer(GravityCompat.START);
+    public RecycleViewCustomAdapter(ArrayList<Comics> arrComics) {
+        this.arrComics = arrComics;
     }
 
     @Override
-    public void onClick(View v) {
-        int id = v.getId();
-        if (id == btn_likes.getId()) {
-            likesClicked();
-        } else if (id == btn_shares.getId()) {
-            sharesClicked();
-        } else if (id == btn_log.getId()) {
-            loginButton.performClick();
-        } else if (id == btn_brightness.getId()) {
-            brightnessClicked();
-        } else if (id == btn_viewMode.getId()) {
-            viewModeClicked();
+    public RecyclerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.view_top_list, parent, false);
+        return new RecyclerViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerViewHolder holder, final int position) {
+        final Context context = holder.comicImage.getContext();
+        holder.comicName.setText(arrComics.get(position).comicsName);
+        holder.viewNumber.setText(arrComics.get(position).getViews() + "");
+        holder.chapterNumber.setText(arrComics.get(position).getEpisodes() + "");
+        Picasso.with(context).load(arrComics.get(position).thumbnail).error(R.mipmap.bia).resize(300, 400).into(holder.comicImage);
+        holder.linear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), ComicDetailActivity.class);
+                intent.putExtra("id", arrComics.get(position).getId() + "");
+                intent.putExtra("comicsName", arrComics.get(position).getComicsName());
+                intent.putExtra("view", String.valueOf(arrComics.get(position).getViews()));
+                intent.putExtra("content", arrComics.get(position).getContent());
+                intent.putExtra("thumbnail", arrComics.get(position).getThumbnail());
+                v.getContext().startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return arrComics.size();
+    }
+
+    public class RecyclerViewHolder extends RecyclerView.ViewHolder {
+        TextView comicName, viewNumber, chapterNumber;
+        ImageView comicImage;
+        LinearLayout linear;
+
+        public RecyclerViewHolder(View itemView) {
+            super(itemView);
+            linear = (LinearLayout) itemView.findViewById(R.id.linear);
+            comicName = (TextView) itemView.findViewById(R.id.txtHeader);
+            comicImage = (ImageView) itemView.findViewById(R.id.imgItem);
+            viewNumber = (TextView) itemView.findViewById(R.id.view_number);
+            chapterNumber = (TextView) itemView.findViewById(R.id.chapter_number);
+
         }
-    }
 
-    public void likesClicked() {
-        facebookAPI.like("252252888524492");
-    }
-
-    public void sharesClicked() {
-
-    }
-
-    public void brightnessClicked() {
-        int currentBrightness = Settings.System.getInt(activity.getContentResolver(), Settings.System.SCREEN_BRIGHTNESS, -1);
-        seekBar.setProgress(currentBrightness);
-        brightnessDialog.show();
-    }
-
-    public void viewModeClicked() {
-        viewModeDialog.show();
-    }
-
-    public void hideActionbar(){
-        toolbar.setVisibility(View.GONE);
-    }
-
-    public SettingHandle getSettingHandle(){
-        return settingHandle;
     }
 }
